b5ce13a7c2caae00bcd7f881b0af1675262ee7e3
iovenkoev <iovenkoev@mail.ru>
2026-04-06 21:20:20 +0300

    Fix syntax error in config

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Контекст",
+    pageTitle: "Контекст моей головы",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
