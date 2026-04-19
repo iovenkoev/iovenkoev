383ca76c90869915b4af8267230329e8c8d8f3f0
iovenkoev <iovenkoev@mail.ru>
2026-04-19 16:31:10 +0300

    vault backup: 2026-04-19 16:31:09

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Контекст моей головы",
+    pageTitle: "Цифровой Сад iovenkoev",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
